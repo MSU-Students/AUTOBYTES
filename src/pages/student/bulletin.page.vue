dfa2209c4e309fbc3884fe33c6f3fb5b10ff58cd
usman12 <usman12strygwyr@gmail.com>
2021-05-23 18:05:00 +0800

    update bulletin (May 23,2021)

--- src/pages/student/bulletin.page.vue
+++ src/pages/student/bulletin.page.vue
@@ -1,14 +1,54 @@
 <template>
   <q-page class="bulletin" padding>
-    <div class="text-h5 text-grey-10">Hello, Atiya Usman!</div>
+    <div>
+      <q-card>
+        <q-tabs
+          v-model="tab"
+          class="bg-primary text-white"
+          active-color="white"
+          indicator-color="white"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="events" label="EVENTS" />
+          <q-tab name="newsandupdates" label="NEWS AND UPDATES" />
+          <q-tab name="achievements" label="ACHIVEMENTS" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated swipeable infinite >
+          <q-tab-panel name="events">
+            <div class="text-h6" style="height: 750px">
+              EVENT ITEMS
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="newsandupdates">
+            <div class="text-h6" style="height: 750px">
+              News and Updates
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="achievements">
+            <div class="text-h6" style="height: 750px">
+              Achievements
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "bulletin"
+  data() {
+    return {
+      tab: "events"
+    };
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
