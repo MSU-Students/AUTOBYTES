e50c102ac6e33e19aa6dcaf1908ac5d25f0ee8c1
usman12 <usman12strygwyr@gmail.com>
2021-06-03 15:48:23 +0800

    update with qlist sample

--- src/pages/student/bulletin.page.vue
+++ src/pages/student/bulletin.page.vue
@@ -11,28 +11,67 @@
           narrow-indicator
         >
           <q-tab name="events" label="EVENTS" />
-          <q-tab name="newsandupdates" label="NEWS AND UPDATES" />
+          <q-tab name="newsandupdates" label="NEWS & UPDATES" />
           <q-tab name="achievements" label="ACHIVEMENTS" />
         </q-tabs>
 
         <q-separator />
 
-        <q-tab-panels v-model="tab" animated swipeable infinite >
+        <q-tab-panels v-model="tab" animated swipeable infinite>
           <q-tab-panel name="events">
             <div class="text-h6" style="height: 750px">
-              EVENT ITEMS
+              <q-list padding>
+                <q-item clickable v-ripple dense>
+                  <q-item-section>
+                    <q-item-label class="text-h6"
+                      >05/12/2021 - Memorandum</q-item-label
+                    >
+                    <q-item-label caption>2nd Semester, From OIPP</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-ripple dense>
+                  <q-item-section>
+                    <q-item-label class="text-h6"
+                      >05/20/2021 - Holiday</q-item-label
+                    >
+                    <q-item-label caption>2nd Semester, From OIPP</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
             </div>
           </q-tab-panel>
 
           <q-tab-panel name="newsandupdates">
             <div class="text-h6" style="height: 750px">
-              News and Updates
+              <q-list padding>
+                <q-item clickable v-ripple dense>
+                  <q-item-section>
+                    <q-item-label class="text-h6"
+                      >05/12/2021 - CSC 121</q-item-label
+                    >
+                    <q-item-label caption
+                      >2nd Semester, From Sir Jojo</q-item-label
+                    >
+                  </q-item-section>
+                </q-item>
+              </q-list>
             </div>
           </q-tab-panel>
 
           <q-tab-panel name="achievements">
             <div class="text-h6" style="height: 750px">
-              Achievements
+              <q-list padding>
+                <q-item clickable v-ripple dense>
+                  <q-item-section>
+                    <q-item-label class="text-h6"
+                      >05/12/2021 - Dance Champion</q-item-label
+                    >
+                    <q-item-label caption
+                      >2nd Semester, From University Foundation</q-item-label
+                    >
+                  </q-item-section>
+                </q-item>
+              </q-list>
             </div>
           </q-tab-panel>
         </q-tab-panels>
